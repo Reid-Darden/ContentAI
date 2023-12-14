0db2a2decd003966bf7beb820adaf590e787cba9
Reid-Darden <reiddarden@gmail.com>
2023-12-14 00:42:24 -0500

    add table data edit cards

--- frontend/js/article.js
+++ frontend/js/article.js
@@ -77,9 +77,36 @@ $(document).ready(async function () {
     // extract paragraphs from input article
     let tempElement = $(article);
 
+    // paragraph data
     let extractedHeaders = tempElement.find(".conseg.outer.s-fit h3");
     let extractedParagraphs = tempElement.find(".conseg.outer.s-fit p");
 
+    // table data
+    let extractedTable = tempElement.find(".table-content");
+    let extractedTableHeaders = extractedTable.find("thead tr");
+    let extractedTableData = extractedTable.find("tbody tr");
+
+    // build structure for table headers - in the event there is more than 1 row of <th>, this checks that the headers pulled match the length of the data it maps too
+    let tableHeaderStructure = [];
+    for (let header of extractedTableHeaders) {
+      let headerRowLength = $(header).find("th").length;
+      let headerRowTh = $(header).find("th");
+
+      let headerRowValue = [];
+      for (let val of headerRowTh) {
+        headerRowValue.push(val.innerHTML);
+      }
+
+      tableHeaderStructure.push({
+        headerRowLength: headerRowLength,
+        headerRowValue: headerRowValue,
+      });
+    }
+
+    let tableStructureMaxLength = tableHeaderStructure.reduce((max, obj) => Math.max(max, obj.headerRowLength), 0);
+    let filteredTableStructure = tableHeaderStructure.filter((obj) => obj.headerRowValue.length == tableStructureMaxLength);
+
+    // build structure for table data that can be edited
     let contentMap = [];
 
     if (extractedHeaders.length == extractedParagraphs.length) {
@@ -91,6 +118,9 @@ $(document).ready(async function () {
       }
     }
 
+    // h3 to start paragraph data edits
+    $("#content_edit_cards").append(`<h3 class="m-3 title is-3 is-align-items-center">Paragraph Data</h3>`);
+
     for (let i = 0; i < contentMap.length; i++) {
       let header = contentMap[i].header;
       let paragraph = contentMap[i].paragraph;
@@ -98,7 +128,7 @@ $(document).ready(async function () {
       let template = `
       <div class="card m-2">
         <header class="card-header">
-          <div class="card-header-title" contentEditable="true" id="card_header">${header}</div>
+          <div class="card-header-title" contenteditable id="card_header">${header}</div>
         </header>
         <div class="card-content">
           <div class="content">
@@ -113,5 +143,65 @@ $(document).ready(async function () {
 
       $("#content_edit_cards").append(template);
     }
+
+    // h3 to start table data
+    $("#content_edit_cards").append(`<h3 class="mr-3 ml-3 mt-5 mb-5 title is-3 is-align-items-center">Table Data</h3>`);
+
+    // build header card from filtered data structure
+    let thOut = "";
+    for (var i = 0; i < filteredTableStructure[0].headerRowLength; i++) {
+      let headerRow = filteredTableStructure[0].headerRowValue[i];
+      thOut += `<td contenteditable style="width: 75px;">${headerRow}</td>`;
+    }
+
+    // append headers card
+    $("#content_edit_cards").append(`
+      <div class="card m-2">
+        <div class="card-content">
+          <div class="content">
+            <table>
+              <tbody>
+                <tr>
+                  ${thOut}
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>`);
+
+    for (let i = 0; i < extractedTableData.length; i++) {
+      let $row = $(extractedTableData[i]);
+
+      let td = $row.find("td");
+
+      let tdOut = "";
+      for (let j = 0; j < td.length; j++) {
+        let rowValue = td[j].innerText;
+        tdOut += `<td contenteditable style="width: 75px;">${rowValue}</td>`;
+      }
+
+      let template = `
+      <div class="card m-2">
+        <div class="card-content">
+          <div class="content">
+            <table>
+              <tbody>
+                <tr>
+                  ${tdOut}
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>`;
+
+      $("#content_edit_cards").append(template);
+    }
+
+    $("#content_edit_cards").append(`
+    <div class="card m-2 has-text-centered">
+      <button class="button m-3 is-dark is-align-items-center" id="card_update_table">Update Table Data</button>
+    </div>`);
   }
 });
